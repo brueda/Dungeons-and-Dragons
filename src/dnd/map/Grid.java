2489fb83cdd6e141db4fe6c5100852c3adb1323d
Ben Rueda <bjrueda@buffalo.edu>
2015-11-14 15:58:13 -0500

    first commit

---
+++ src/dnd/map/Grid.java
@@ -0,0 +1,10 @@
+package dnd.map;
+
+public class Grid {
+	private Tile[][] _grid;
+	
+	public Grid(int size){
+		_grid = new Tile[size][size];
+	}
+
+}
---
+++ src/dnd/map/Tile.java
@@ -0,0 +1,21 @@
+package dnd.map;
+
+
+public class Tile {
+	private final int LEFT = 0;
+	private final int RIGHT = 1;
+	private final int UP = 2;
+	private final int DOWN = 3;
+	
+	private boolean _walls[];
+	
+	public Tile(){
+		_walls = new boolean[4];
+	}
+	
+	
+	
+	
+	
+
+}
